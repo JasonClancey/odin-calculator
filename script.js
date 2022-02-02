bd17404973030d61bc3556894b5de992175452df
Jason Clancey <jason.clancey@dal.ca>
2022-02-01 20:05:11 -0700

    added functionality to click numbers and display them, clear the numbers, and select an operator

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-let displayValue
+let display = '';
+let num1;
+let num2;
+let chosenOperator;
+
 
 function add(num1, num2) {
     return num1 + num2;
-}
+};
 
 function subtract(num1, num2) {
     return num1 - num2;
-}
+};
 
 function multiply(num1, num2) {
     return num1 * num2;
-}
+};
 
 function divide(num1, num2) {
     if (num2 === 0) {
@@ -18,21 +22,51 @@ function divide(num1, num2) {
     }
     else { return num1 / num2;
     }
-}
+};
 
 function operate(operator, num1, num2) {
-    if (operator === '+') {
+    if (chosenOperator === '+') {
         display = add(num1, num2);
     }
-    if (operator === '-') {
+    if (chosenOperator === '-') {
         display = subtract(num1, num2);
     }
-    if (operator === '*') {
+    if (chosenOperator === '*') {
         display = multiply(num1, num2)
     }
-    if (operator === '/') {
+    if (chosenOperator === '/') {
         display = divide(num1, num2)
     }
     return display
-}
+};
+
+const displayValue = document.querySelector('.display');
+
+function refreshDisplay() {
+    displayValue.textContent = `${display}`
+};
+
+const numbers = document.querySelectorAll('.number');
+numbers.forEach((number => {
+    number.addEventListener('click', () => {
+        display += number.value;
+        refreshDisplay();
+    });
+}));
+
+const clear = document.querySelector('.clear')
+clear.addEventListener('click', () => {
+    display = '';
+    refreshDisplay();
+});
+
+const selectOperator = document.querySelectorAll('.operator');
+selectOperator.forEach((operator => {
+    operator.addEventListener('click', () => {
+        chosenOperator = operator.id;
+        num1 = display;
+        display = '';
+        refreshDisplay();
+    });
+}))
 
